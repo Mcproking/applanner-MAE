1fad9a5de463172bde65fc7596fec8eb4b447d93
Mcproking <shanefoomg@gmail.com>
2025-05-29 03:27:44 +0800

    update to make sure is scroable

--- lib/user_management/user_management_list.dart
+++ lib/user_management/user_management_list.dart
@@ -48,108 +48,115 @@ class _UserManegementState extends State<UserManegementList> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ...List.generate(_listButton.length, (index) {
-            final item = _listButton[index];
-            return _buildCard(item['icon'], item['label'], item['redirect']);
-          }),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+        backgroundColor: Color.fromARGB(255, 51, 51, 51),
+        shadowColor: Color.fromARGB(255, 119, 119, 119),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...List.generate(_listButton.length, (index) {
+              final item = _listButton[index];
+              return _buildCard(item['icon'], item['label'], item['redirect']);
+            }),
 
-          // Logout Button
-          GestureDetector(
-            onTap: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Colors.redAccent.shade200,
-                  content: Row(
-                    children: [
-                      Expanded(
-                        child: const Text(
-                          "Proceed To Logout?",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                          FirebaseAuth.instance.signOut().then((val) {
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const LoginPage(),
-                              ),
-                              (Route<dynamic> route) => false,
-                            );
-                          });
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: const Text(
-                                'Account logged out successfully.',
-                              ),
-                              backgroundColor: Colors.green,
-                            ),
-                          );
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 5,
-                            horizontal: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.redAccent.shade100,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black,
-                                offset: Offset(0, 2),
-                                blurRadius: 5,
-                                blurStyle: BlurStyle.inner,
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            "Logout",
+            // Logout Button
+            GestureDetector(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    backgroundColor: Colors.redAccent.shade200,
+                    content: Row(
+                      children: [
+                        Expanded(
+                          child: const Text(
+                            "Proceed To Logout?",
                             style: TextStyle(
-                              color: Colors.white,
                               fontSize: 20,
-                              fontWeight: FontWeight.w500,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        GestureDetector(
+                          onTap: () {
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                            FirebaseAuth.instance.signOut().then((val) {
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginPage(),
+                                ),
+                                (Route<dynamic> route) => false,
+                              );
+                            });
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: const Text(
+                                  'Account logged out successfully.',
+                                ),
+                                backgroundColor: Colors.green,
+                              ),
+                            );
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.redAccent.shade100,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black,
+                                  offset: Offset(0, 2),
+                                  blurRadius: 5,
+                                  blurStyle: BlurStyle.inner,
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 15,
+                    ),
+                    duration: const Duration(milliseconds: 3000),
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 30,
-                    horizontal: 15,
-                  ),
-                  duration: const Duration(milliseconds: 3000),
+                );
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 100),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.redAccent.shade400,
                 ),
-              );
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 100),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.redAccent.shade400,
-              ),
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
-              child: Row(
-                children: [
-                  Icon(Icons.logout, size: 35),
-                  const SizedBox(width: 10),
-                  Text("Logout", style: TextStyle(fontSize: 24)),
-                ],
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
+                child: Row(
+                  children: [
+                    Icon(Icons.logout, size: 35),
+                    const SizedBox(width: 10),
+                    Text("Logout", style: TextStyle(fontSize: 24)),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
